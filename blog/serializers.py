7f1d100006c49a84fb862eccbb0d7af6dc04ec80
Chenlei Fu <chenlei.fu@outlook.com>
2020-08-19 00:24:34 -0500

    requirements added

---
+++ blog/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from .models import Post, Comment
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('id','title', 'slug','author','updated_on','content','created_on','status')
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('id','post', 'name','email','body','created_on','active')
